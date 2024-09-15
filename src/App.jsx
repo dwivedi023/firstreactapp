d0c87ea13deb5ce861811c3723d9cc702ae9c92c
Adarsh Dwivedi <dwivediadarsh074@gmail.com>
2024-09-15 18:56:45 +0530

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 // import React from 'react'
+import './index.css'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div className='m-5'>
+      <h1 className='font-bold uppercase ' >ram ram ji</h1><br />
+      {import.meta.env.VITE_API_KEY} <br />
+  <button className =" mt-5 explore-btn relative px-8 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg shadow-md transition-transform duration-300 hover:scale-105 focus:outline-none">
+        Explore
+    </button>
+    </div>
   )
 }
 
