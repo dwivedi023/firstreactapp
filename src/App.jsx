e74f7c86c158f8586376d7127d257b961b90f734
Adarsh Dwivedi <dwivediadarsh074@gmail.com>
2024-09-15 19:04:28 +0530

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './index.css'
 export const App = () => {
   return (
     <div className='m-5'>
-      <h1 className='font-bold uppercase ' >ram ram ji</h1><br />
+      <h1 className='font-bold uppercase ' >ram ram ji🙏</h1><br />
       {import.meta.env.VITE_API_KEY} <br />
   <button className =" mt-5 explore-btn relative px-8 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg shadow-md transition-transform duration-300 hover:scale-105 focus:outline-none">
         Explore
